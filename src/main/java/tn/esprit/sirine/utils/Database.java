658f7e13013c801e8f7d2e2790f0c4b5abff1122
sirine-baraket <sirine.baraket@esprit.tn>
2025-05-11 14:00:23 +0200

    dependency

--- src/main/java/tn/esprit/sirine/utils/Database.java
+++ src/main/java/tn/esprit/sirine/utils/Database.java
@@ -1,10 +1,11 @@
 package tn.esprit.sirine.utils;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Database {
+
+
     private static final String URL = "jdbc:mysql://localhost:3306/publication_db";
     private static final String USER = "root";
     private static final String PASSWORD = "";
